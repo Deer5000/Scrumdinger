0a85d598981fbe50a637409763ca515ffc981fa1
Deer5000 <khidrbrinkley32@gmail.com>
2021-07-28 18:27:17 -0400

    first commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Khidr Brinkley on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
