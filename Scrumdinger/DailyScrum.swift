ebbfed82b74dc176bc6256f263f5cf4492de1d32
Deer5000 <khidrbrinkley32@gmail.com>
2021-07-30 08:17:37 -0400

    Finished Daiy Scrum Model

--- Scrumdinger/DailyScrum.swift
+++ Scrumdinger/DailyScrum.swift
@@ -17,7 +17,12 @@ struct DailyScrum {
 
 extension DailyScrum {
     static var data: [DailyScrum] {
-        DailyScrum(title: "Design", attendees: ["", "", "", ""], lengthInMinutes: 10, color: Color("Design"))
+        [
+            
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color("Design")),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christian", "Eden", "Karla","Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
+        ]
     }
     
 }
