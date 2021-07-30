624579690894d733aec9d6a00425a53c2d442c03
Deer5000 <khidrbrinkley32@gmail.com>
2021-07-30 09:27:10 -0400

    Card View Created

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,40 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Khidr Brinkley on 7/30/21.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title).font(.headline)
+        Spacer()
+        HStack() {
+            Label("\(scrum.attendees.count)", systemImage: "person.3")
+            Spacer()
+            Label("\(scrum.lengthInMinutes)", systemImage: "clock").padding(.trailing, 20)
+            }
+        .font(.caption)
+        }
+        .padding()
+        ///.foregroundColor(scrum.color.accesibleFontColor)
+        .foregroundColor(Color.black.colorInvert() as? Color)
+//        .foregroundColor(.black)
+    }
+}
+
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.data[0]
+    static var previews: some View {
+        CardView(scrum:scrum)
+            .background(scrum.color)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
+
+
